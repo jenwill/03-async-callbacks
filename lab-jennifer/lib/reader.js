cac8ee0a60d341af6599853e76e0c1e77de57ec4
Jennifer Williams <jennifer@squareandplum.com>
2018-04-18 16:31:48 -0700

    reorder file system, install winston, set up logger

---
+++ lab-jennifer/lib/reader.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const fs = require('fs');
+const logger = require('./logger');
+
+const reader = module.exports = {};
---
+++ lab-jennifer/main.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const logger = require('./lib/logger');
+const reader = require('./lib/reader');
+
+logger.log(logger.INFO, 'I am a winston log');
