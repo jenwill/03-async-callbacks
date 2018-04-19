c874060f7ff8c6ac93d9cdc4b517433a595508d2
Jennifer Williams <jennifer@squareandplum.com>
2018-04-18 17:49:46 -0700

    add data files, create reader module, set up basic and main.js code to invoke reader and print text to the console

--- lab-jennifer/lib/reader.js
+++ lab-jennifer/lib/reader.js
@@ -4,3 +4,14 @@ const fs = require('fs');
 const logger = require('./logger');
 
 const reader = module.exports = {};
+
+reader.readNCharsAsync = (filePath, characters, callback) => {
+  logger.log(logger.VERBOSE, `Reading ${filePath}`);
+
+  fs.readFile( filePath, (error, fileBuffer) => {
+    if (error) {
+      throw error;
+    }
+    return callback(fileBuffer.toString('utf8', 0, characters));
+  });
+};
